5671147ae8f153c936cc654c5093d6d4b1b35558
dvelal <dvelal@unjbg.edu.pe>
2025-06-20 21:33:08 -0500

    Ejercicio 3: Dia con menor temperatura, agregue restricciones

--- arreglos3.cpp
+++ arreglos3.cpp
@@ -8,11 +8,25 @@ int main() {
         cout<<"-- Dia "<<i+1<<" --"<<endl;
         cout<<"Temperatura minima: "; cin>>DiasMin[i];
         cout<<"Temperatura maxima: "; cin>>DiasMax[i];
+        if (DiasMin[i] > DiasMax[i]) {
+            cout<<"\nERROR al ingresar los datos";
+            return 1;
+        }
     }
     cout<<"--Temperatura media de cada dia--"<<endl;
     for (int i = 0; i < 7; i++) {
         float Prom = (DiasMin[i] + DiasMax[i])/2;
         cout<<"Dia "<<i+1<<": "<<Prom<<endl;
+    }
+    float min = DiasMin[0];
+    cout<<"--Dia(s) con la menor temperatura--"<<endl;
+    for (int i = 0; i < 7; i++) {
+        if (DiasMin[i] < min) 
+            min = DiasMin[i];
+    }
+    for (int i = 0; i < 7; i++) {
+        if (min == DiasMin[i])
+            cout<<"Dia "<<i+1<<endl;
     }
     return 0;
 }
