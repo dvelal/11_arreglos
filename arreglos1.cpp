deeb8512fa4c24678faa7c6a3d78ab904907127f
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-07-09 20:12:03 -0500

    Agrega función al arreglo1.cpp : proceso

--- arreglos1.cpp
+++ arreglos1.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void proceso (int &, int &, int &);
+
 int main() {
-    int n, s, c; 
+    int n, s, c;
     cout<<"Ingrese la cantidad de elementos: "; cin>>n;
     int A[n];
     s = 0;
     c = 0;
+
     for (int i = 0; i < n; i++) {
         cout<<"Ingrese el elemento "<<i+1<<": "; cin>>A[i];
-        s = s + A[i];
-        if ( A[i] == 0 )
-            c = c + 1;
+        proceso(A[i], s, c);
     }
-    float porc = (c*100)/n;
+    float porc = (c*100.0)/n;
     cout<<"Sumatoria de elementos: "<<s<<endl;
-    cout<<"porcentaje de ceros: "<<porc<<"%";
+    cout<<"Porcentaje de ceros: "<<porc<<"%";
     return 0;
+}
+
+void proceso (int &A,int &s, int &c){
+    s = s + A;
+        if (A == 0){
+            c = c + 1;
+        }
 }
